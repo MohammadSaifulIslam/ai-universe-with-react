c6f350a3ebd5f1edbce9dc0cc0dc29f6cbc8f0ed
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-03-26 19:04:06 +0600

    project setup and tailwind and daisyui install

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './assets/components/Button/Button';
+import Header from './assets/components/Header/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Button>Sort By Date</Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='text-center'>
+            <h1 className='text-4xl font-bold my-5'>AI Universe Hub</h1>
+        </div>
+    );
+};
+
+export default Header;
